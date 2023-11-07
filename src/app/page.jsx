df76dcd95099e4b4f73b2d8f77d6e78faa9e9374
jeet <jeetp105520@gmail.com>
2023-11-07 14:56:06 +0530

    final-2

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,16 +4,13 @@ import Featured from "@/components/featured/Featured";
 import CategoryList from "@/components/categoryList/CategoryList";
 import CardList from "@/components/cardList/CardList";
 import Menu from "@/components/menu/Menu";
-import Main from "@/components/main/Home";
 
 export default function Home({searchParams}) {
 
   const page = parseInt(searchParams.page) || 1;
 
   return (
-    
     <div className={styles.container}>
-      {/* <Main /> */}
       <Featured />
       <CategoryList />
       <div className={styles.content}>
